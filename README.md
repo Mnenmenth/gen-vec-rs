405ce3020c513a8beca2df7793cc8ab4d0d9010d
Earl Kennedy <mnenmenth@gmail.com>
2020-07-14 18:08:53 -0400

    Added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+ # gen-vec
+ 
+ Vector of reusable, generational indices that owns its values
+
+ [Inspired by Catherine West's closing keynote at RustConf 2018](https://kyren.github.io/2018/09/14/rustconf-talk.html)
+
+ ### Closed vs. Exposed index allocation implementations
+
+ `ClosedGenVec` uses a non user-accessible index allocator to manage indices
+
+ `ExposedGenVec` relies on an external `IndexAllocator`
+
+ As such, an `IndexAllocator` must be created and used to allocate/deallocate indices manually.
+ This is useful for using the same `Index` across multiple `ExposedGenerationalVec` instances
+
+ **Note:** `IndexAllocator` cannot be used with `ClosedGenerationalVec` since it has its own 
+ internal `IndexAllocator`
+
+ ### Explanation of Generational Indices
+
+ `Index` structs are used to access the vector's contents. An `Index` contains an index for the vector
+ and a generation (which is 0 initially).
+
+ Deallocated/removed `Index`s go into a list of free `Index`s that can be reused
+
+ Every time an `Index` is reused, the internal generation is incremented. This ensures that a deallocated
+ `Index` handle can't access data that it no longer validly points to
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-//! Vector of reusable, generational indices that owns the contained values
+//! # gen-vec
+//!
+//! Vector of reusable, generational indices that owns its values
 //!
 //! [Inspired by Catherine West's closing keynote at RustConf 2018](https://kyren.github.io/2018/09/14/rustconf-talk.html)
 //!
 //! ## Closed vs. Exposed index allocation implementations
 //!
-//! `ClosedGenVec` uses a non user-accessible index allocator to manage `Index`s
+//! `ClosedGenVec` uses a non user-accessible index allocator to manage indices
 //!
 //! `ExposedGenVec` relies on an external `IndexAllocator`
 //!
-//! As such, an `IndexAllocator` must be created and used to allocate/deallocate `Index`s manually.
-//! This is useful for using the same `Index` across multiple `ExposedGenerationalVec`s
+//! As such, an `IndexAllocator` must be created and used to allocate/deallocate indices manually.
+//! This is useful for using the same `Index` across multiple `ExposedGenerationalVec` instances
 //!
-//! **Note:** `IndexAllocator` cannot be used with the self-allocating `ClosedGenerationalVec` since
-//! it manages its own `Index`s
+//! **Note:** `IndexAllocator` cannot be used with `ClosedGenerationalVec` since it has its own
+//! internal `IndexAllocator`
 //!
 //! ## Explanation of Generational Indices
 //!
